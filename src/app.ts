02bba48083be4d3906a8d289d1df507d013231ba
surajsgh <surajsingh220698@outlook.com>
2023-11-26 16:52:47 +0530

    Add basic middlewares.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,30 @@
 import express, { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+
+declare global {
+  namespace Express {
+    interface Request {
+      requestTime: number;
+    }
+  }
+}
 
 const app = express();
+
+//  MIDDLEWARES
+app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log("logging...");
+  next();
+});
+
+app.use(morgan("dev"));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.requestTime = Date.now();
+  next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Hello, world!" });
