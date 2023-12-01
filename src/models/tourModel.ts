c87ab4bd47884567a3e0ac0c0b97a4c2bdc671e2
surajsgh <surajsingh220698@outlook.com>
2023-12-01 20:05:33 +0530

    Add a virtual property.

--- src/models/tourModel.ts
+++ src/models/tourModel.ts
@@ -1,32 +1,40 @@
 import mongoose from 'mongoose';
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name.'],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name.'],
+      unique: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price.'],
+    },
+    description: {
+      type: String,
+      required: [true, 'A tour must have a description.'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    hotels: [String],
+    tourGuide: [String],
+    durations: {
+      type: String,
+      required: [true, 'A tour must have a duration.'],
+    },
+    places: [String],
   },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price.'],
-  },
-  description: {
-    type: String,
-    required: [true, 'A tour must have a description.'],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  hotels: [String],
-  tourGuide: [String],
-  durations: {
-    type: String,
-    required: [true, 'A tour must have a duration.'],
-  },
-  places: [String],
-});
+  { toJSON: { virtuals: true } },
+);
+
+// Virtual Query
+tourSchema
+  .virtual('expiresIn')
+  .get(() => `${Math.floor(Math.random() * 10) + 1} days`);
 
 const Tour = mongoose.model('Tour', tourSchema);
 
