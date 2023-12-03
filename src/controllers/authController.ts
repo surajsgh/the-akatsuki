dfa750779fe5ab39a4c2450ad0fc2149bd19dae4
surajsgh <surajsingh220698@outlook.com>
2023-12-03 17:46:09 +0530

    refactor get token helper function.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,16 +2,20 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
+import { Types } from 'mongoose';
 import User from '../models/userModel.ts';
 import catchAsync from '../utils/catchAsync.ts';
 import AppError from '../utils/appError.ts';
 
+const getToken = (id: Types.ObjectId): string =>
+  jwt.sign({ id }, process.env.SECRET_KEY!, {
+    expiresIn: process.env.EXPIRES_IN,
+  });
+
 export const signup = catchAsync(async (req: Request, res: Response) => {
   const user = await User.create(req.body);
 
-  const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY!, {
-    expiresIn: process.env.EXPIRES_IN,
-  });
+  const token = getToken(user._id!);
 
   return res
     .status(200)
@@ -27,8 +31,6 @@ export const login = catchAsync(
 
     const registeredUserPassword = await User.findOne({ email });
 
-    console.log(registeredUserPassword);
-
     if (!registeredUserPassword)
       return next(new AppError('This user is not registered.', 400));
 
@@ -37,13 +39,7 @@ export const login = catchAsync(
     if (!match)
       return next(new AppError('Oops! Entered password is wrong.', 400));
 
-    const token = jwt.sign(
-      { id: registeredUserPassword._id },
-      process.env.SECRET_KEY!,
-      {
-        expiresIn: process.env.EXPIRES_IN,
-      },
-    );
+    const token = getToken(registeredUserPassword._id);
 
     return res.status(200).json({
       error: true,
